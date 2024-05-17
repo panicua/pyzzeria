17b43d30be8f7e899ba8881164fbb3f07e6b85f6
kekW <panicua12@gmail.com>
2024-05-17 15:38:04 +0300

    feat: add static MEDIA_URL, MEDIA_ROOT to urlpatterns

--- pyzzeria/urls.py
+++ pyzzeria/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pizza_delivery.urls", namespace="pizza_delivery")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
